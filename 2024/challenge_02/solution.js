918d91e0b95b0b18a7455a1d792645a5fd850ede
Daniel Muelle <danimr1999@gmail.com>
2024-12-01 20:29:30 +0100

    feat: challenge 2 2024

---
+++ 2024/challenge_02/solution.js
@@ -0,0 +1,69 @@
+import fs from "fs/promises";
+
+async function readFile() {
+  try {
+    return await fs.readFile("log.txt", "utf8");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function isDigit(char) {
+  return /^[0-9]$/.test(char);
+}
+
+function isLowercaseLetter(char) {
+  return /^[a-z]$/.test(char);
+}
+
+function validateString(input) {
+  let seenLetter = false;
+  let previousLetter = "";
+  let previousDigit = -1;
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (isDigit(char)) {
+      const currentDigit = parseInt(char);
+
+      if (seenLetter || currentDigit < previousDigit) {
+        return false;
+      }
+
+      previousDigit = currentDigit;
+    } else if (isLowercaseLetter(char)) {
+      if (seenLetter && char < previousLetter) {
+        return false;
+      }
+
+      previousLetter = char;
+      seenLetter = true;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+async function getSolution() {
+  const log = await readFile();
+  const formattedLogs = log.split("\n");
+
+  let validAttempts = 0;
+  let invalidAttempts = 0;
+
+  for (const attempt of formattedLogs) {
+    if (validateString(attempt)) {
+      validAttempts++;
+    } else {
+      invalidAttempts++;
+    }
+  }
+
+  return `${validAttempts}true${invalidAttempts}false`;
+}
+
+const result = await getSolution();
+console.log(result);
